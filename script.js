6694766bbba28b2b9e72658f75dd4c67f33a75bd
Fabiano <binobrenda2016@gmail.com>
2023-11-22 17:21:38 -0300

    Melhorando o código

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let num1= prompt("digite um numero:");
+/*let num1= prompt("digite um numero:");
 
 let num2 = prompt("Digite outro numero:")
 
@@ -18,4 +18,53 @@ soma = Number(num1) +Number(num2);
 
 
 
-alert ('O resultado da op ê' +soma);
+/*alert ('O resultado da op ê' +soma);
+
+let n1 = prompt("Informe a primeira nota");
+let n2 =prompt("Informe a segunda nota");
+let n3 = prompt("Informe a terceira nota");
+let n4 = prompt("Informe a quarta nota");
+let media;
+
+media = (Number(n1) + Number(n2) + Number(n3) + Number(n4))/4
+
+alert ("o resultado da media");
+/* A locodora de carros prescisa da sua ajuda para cobrar seus serviços.Escreva um programa que pergunte a qtde. de Km percorridos por um carro alugado e a qtde. dfe dias pelos quais ele foi alugado calcule o preço total a pagar sabendo que o carro cust R$ 90 por dia 
+e R$ 0,20 por KM rodado.*/
+
+ /*let km = prompt("digite a quantidade de km percorrida ");
+ let dias = prompt("digite a quantidade de dias  utilizando o produto");
+ 
+
+ let(soma);
+  soma = Number(km)*0.20; +(Number(dias)*90);
+alert ('o preço total é R$' +soma);
+ /* Faça um algoritmo que pergunte a distância que um passageiro deseja percorrer em km .Calcule o preço da passagem ,cobrando R$0.50 por Km parea viagens até 
+ 200 Km e R$0.45 para viagens mais longas.*/
+
+ /*let distância =prompt("Digite a distância percorrida");
+  let total;
+  if (dist <=200){
+          total = Number(distancia)*0.50;}
+          {else{
+                  total =Number(distância)*0.45;
+                alert("totala pagar:" + total)
+          }
+          ]
+  }
+  /* Crie um programaque leia duas notas de um aluno e calcule a sua media ,mostrando uma mensagem no final,de acordo com a media atingida:
+  -media até 4.9 REPROVADO
+  -média de 5.0 e 6.9 RECUPERAÇÃO 
+  -Média entre 7.0 ou superior APROVADO.*/
+  
+  let n1 = prompt("Informe a primeira nota");
+  let n2 = prompt("informe a segunda nota ");
+  let media;
+
+ if(media<=4.9) {
+        alert ("reprovado")
+}else if(media >=5 && media <=6.9){
+        alert("recuperação");
+}else {
+        alert("aprovado");
+}
